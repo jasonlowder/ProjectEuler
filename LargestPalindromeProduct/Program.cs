ea230f9cdecd34ddf1523320291c41601d4329b2
Jason Lowder <jlowder@cricut.com>
2016-02-07 15:41:20 -0700

    Add LargestPalindromeProduct

---
+++ LargestPalindromeProduct/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LargestPalindromeProduct
+{
+    /// <summary>
+    /// A palindromic number reads the same both ways.
+    /// The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+    /// Find the largest palindrome made from the product of two 3-digit numbers.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Palindromes(100,999).Max());
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> Palindromes(int lower, int upper)
+        {
+            for (var i = lower; i <= upper; i++)
+            {
+                for (var j = lower; j <= upper; j++)
+                {
+                    var product = i*j;
+                    if (IsPalidrome(product))
+                    {
+                        yield return (product);
+                    }
+                }
+            }
+        }
+
+        static bool IsPalidrome(int candidant)
+        {
+            return candidant == int.Parse(new string(candidant.ToString().Reverse().ToArray()));
+        }
+    }
+}
